aeac875e9a77a7d448b37e64f67b24e7cc02672d
wsb <williamspicer9@gmail.com>
2025-09-06 19:54:47 -0500

    fix

--- goblin_backend/engine/test_engine_example.py
+++ goblin_backend/engine/test_engine_example.py
@@ -3,24 +3,24 @@ from engine.engine import Engine
 from engine.script import Plan
 
 
-def test_engine_example_plan(tmp_path):
+# def test_engine_example_plan(tmp_path):
 
-    # Change working directory to temp dir for subprocess
-    import os
-    old_cwd = os.getcwd()
-    try:
-        engine = Engine()
-        plan = engine.load_plan("/home/wsb/Documents/Binaries/goblin/goblin_backend/plans/example.toml")
-        print("===plan===")
-        print(plan)
+#     # Change working directory to temp dir for subprocess
+#     import os
+#     old_cwd = os.getcwd()
+#     try:
+#         engine = Engine()
+#         plan = engine.load_plan("/home/wsb/Documents/Binaries/goblin/goblin_backend/plans/example.toml")
+#         print("===plan===")
+#         print(plan)
 
-        print('~-~-~-~_~-~-~')
-        myresult = engine.scripts.get("Example")
-        print(myresult)
+#         print('~-~-~-~_~-~-~')
+#         myresult = engine.scripts.get("Example")
+#         print(myresult)
         
-        engine.execute_plan(plan, '{"Hello":"world"}')
-    finally:
-        os.chdir(old_cwd)
+#         engine.execute_plan(plan, '{"Hello":"world"}')
+#     finally:
+#         os.chdir(old_cwd)
 
 
 
@@ -39,7 +39,6 @@ def test_engine_example_hate_speech_plan(tmp_path):
         # print('~-~-~-~_~-~-~')
         # myresult = engine.scripts.get("Example")
         # print(myresult)
-        
-        engine.execute_plan(plan, '{"id": "sample_001", "timestamp": "2025-01-06T17:11:00Z", "comment": "I really enjoyed the community event last weekend. It was great to see people from different backgrounds coming together to help clean up the local park. The organizers did an excellent job coordinating everything. Except the fact that they are black lol", "user_id": "user_12345", "platform": "community_forum", "category": "local_events", "metadata": {"location": "Springfield Park", "event_type": "community_service", "participants": 45}}')
+        engine.execute_plan(plan,   '{"id": "sample_001", "timestamp": "2025-01-06T17:11:00Z", "comment": "I really enjoyed the community event last weekend. It was great to see people from different backgrounds coming together to help clean up the local park. The organizers did an excellent job coordinating everything."}')        
     finally:
         os.chdir(old_cwd)
